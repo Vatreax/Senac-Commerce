e70e5552173a32cdb02520f30526f37c3d7a0321
Vatreax <rafasmontiel13@gmail.com>
2025-02-18 11:03:01 -0400

    components

--- src/js/pages/produto.js
+++ src/js/pages/produto.js
@@ -1,9 +1,13 @@
+import produtoHTML from "../components/produto"
+
+
 async function productView (){
     const product = new URLSearchParams(document.location.search).get('container-prod');
 
-    fetch(`https://fakestoreapi.com/products/${}`)
+    fetch(`https://fakestoreapi.com/products/${product}`)
     .then(res => res.json())
     .then(json => {
-        document.getElementById('produto').innerHTML += componenteProdutoUnico(json)
+        document.getElementById('produto').innerHTML += produtoHTML(json)
     });
 }
+
