7c3650b0322273feebda7b002005c938cb31a882
Felipe Gustavo Braiani Santos <felipe.braianitech@gmail.com>
2025-02-18 00:13:29 -0400

    Adicionado chamada à API

--- src/js/pages/usuario.js
+++ src/js/pages/usuario.js
@@ -6,7 +6,9 @@ if(userId === null || userId === undefined){
     //window.location.href = '/login.html';
 }
 
+async function getUserFromApi(id) {
+    return await fetch('https://fakestoreapi.com/users/' + id)
+        .then(res=>res.json());
+}
 
-document.getElementById('app').innerHTML = usuarioForm()
-
-console.log(userId);
+document.getElementById('app').innerHTML = usuarioForm(getUserFromApi(1))
