c9f05a2f1141fd55893a909c3742742a29927b7b
Carlos Eduardo De L. G. <105987211+cadulucas1@users.noreply.github.com>
2025-02-18 11:04:05 -0400

    mudanças 1802

---
+++ loginpage.js
@@ -0,0 +1,28 @@
+import Loginz from "./loginform";
+
+
+async function pesquisarUser(){
+
+};
+
+async function validarUser(){
+// add algo para salvar o id
+};
+
+async function sessionUser(){
+
+};
+
+async function FecthLogin(){
+
+};
+
+function enviarLogin(event){
+    event.preventdefault();
+
+    const username = event.target.loginz.value;
+    const senha = event.target.senha.value;
+};
+
+document.getElementById('login')
+
